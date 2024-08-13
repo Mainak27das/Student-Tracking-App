04e63fadc252250444b4a48569cbe1edd71eef17
My-new-Repository <debnathrudradeep@gmail.com>
2024-08-13 18:22:55 +0530

    Message and search added

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from myapp import views
 
 urlpatterns = [
-   path('',views.index,name='home'),
+   path('',views.index,name='index'),
    path('add/', views.add_student, name='add_student'),
    # path('edit/<int:student_id>/', views.edit_student, name='edit_student'),
+   path("search", views.search, name="search"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,31 +1,50 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Student
 from .forms import StudentForm
-
-# def index(request):
-#     return render(request,'index.html')
+from django.contrib import messages
 
 def index(request):
     students = Student.objects.all()
-    return render(request, 'index.html', {'students': students})
+    total_students = students.count()
+    cbse_students = students.filter(board='CBSE').count()
+    wbbse_students = students.filter(board='WBBSE').count()
+    icse_students = students.filter(board='ICSE').count()
+
+    context = {
+        'students': students,
+        'total_students': total_students,
+        'cbse_students': cbse_students,
+        'wbbse_students': wbbse_students,
+        'icse_students': icse_students,
+    }
+    return render(request, 'index.html', context)
 
 def add_student(request):
     if request.method == 'POST':
         form = StudentForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('home')
+            messages.success(request, " Student Added !! ")
+            return redirect('index')
     else:
         form = StudentForm()
     return render(request, 'add_student.html', {'form': form})
 
-# def edit_student(request, student_id):
-#     student = get_object_or_404(Student, id=student_id)
-#     if request.method == 'POST':
-#         form = StudentForm(request.POST, instance=student)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('home')
-#     else:
-#         form = StudentForm(instance=student)
-#     return render(request, 'edit_student.html', {'form': form, 'student': student})
+
+def search(request):
+    search = request.GET.get("search", "")
+    
+    if len(search) > 80:
+        all_students = Student.objects.none()
+    else:
+        # Filter based on available fields in the Student model
+        all_students_name = Student.objects.filter(name__icontains=search)
+        all_students_school = Student.objects.filter(school__icontains=search)
+        all_students_board = Student.objects.filter(board__icontains=search)
+        all_students_class = Student.objects.filter(student_class__icontains=search)
+        
+        # Combine all querysets
+        all_students = all_students_name.union(all_students_school, all_students_board, all_students_class)
+    
+    context = {"students": all_students, "search": search}
+    return render(request, "search.html", context)

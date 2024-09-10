a73f8c37eea74bce610906e75bc1e3119b628c00
Soumen3 <soumensamanta112233@gmail.com>
2024-09-10 22:41:59 +0530

    Forgot Password

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from myapp import views
 from .views import create_batch, view_batches, BatchDetailView, add_existing_students, add_new_student,edit_teacher, delete_teacher
@@ -39,6 +39,13 @@ urlpatterns = [
 
     path('accounts/login/', views.user_login, name='login'),    
     path('accounts/logout/', views.user_logout, name='logout'),
+	
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
 ]
 
 

9041f7a1b7a07190eebf6df7c85ae97f28581ce7
Mainak Das <dasmainak2018@gmail.ccom>
2024-09-24 00:56:39 +0530

    dubugged button

--- static/js/class_details.js
+++ static/js/class_details.js
@@ -1,10 +1,17 @@
-
+console.log('fuck')
 // button toggle animation
-document.querySelector('#exploreClassesBtn').addEventListener('click', function () {
-    var buttons = document.querySelectorAll('#exploreIcons .social-btn');
-    buttons.forEach(function (button, index) {
-        button.style.animationDelay = (index * 0.2) + 's'; // Stagger delay by 0.2s for each button
+window.onload=function(){
+    console.log('fucku')
+    document.querySelector('#exploreClassesBtn').addEventListener('click', function () {
+        console.log('fuckme')
+        var buttons = document.querySelectorAll('#exploreIcons .social-btn');
+    
+        buttons.forEach(function (button, index) {
+            console.log(button)
+            button.style.animationDelay = (index * 0.2) + 's'; // Stagger delay by 0.2s for each button
+        });
     });
-});
+    
+}
 
 // -----------
